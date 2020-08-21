9052689657f55b62a3626503445ddc5859c9eb07
raja <rajamanikumar662@gmail.com>
2020-08-21 22:42:48 +0530

    added voice command

--- human.py
+++ human.py
@@ -1,6 +1,25 @@
 import pyttsx3
 import os 
 import time
+import speech_recognition as sr
+import pyaudio
+
+def listening():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+
+        print("listening.....")
+        audio=r.listen(source)
+    try:
+        voice=r.recognize_google(audio, language=' en-in')
+        print(f"you said {voice}.")
+    except:
+        print("sorry I cant recognize...")
+        pyttsx3.speak("sorry I cant recognize..., can you say that again")
+        return 'None'
+    return voice
+        
 
 pyttsx3.speak("hi BOSS")
 while True:
@@ -9,9 +28,9 @@ while True:
     print()
     pyttsx3.speak("what can i do for u: ")
 
-    p=input()
+    p=listening().lower()
     
-    if "dont" in p and ("chrome" in p or "notepad" in p or "player" in p or "editor" in p):
+    if "don't" in p and ("chrome" in p or "notepad" in p or "player" in p or "editor" in p):
         pyttsx3.speak("ok then iam not running ")
     elif ("run" in p or "open" in p) and "chrome" in p:
         os.system("chrome")
@@ -25,7 +44,7 @@ while True:
         pyttsx3.speak("ok byee,       see u later.")
         break
     else:
-        pyttsx3.speak("sorry something went wrong, try again")
+        pyttsx3.speak("check your internet conection and try again")
 
 
 
